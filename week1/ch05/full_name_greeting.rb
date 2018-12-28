29274c6f636ff284e88dd8bb459dfdcd38f4b9a2
scarlett7x <scarlett7x@hotmail.com>
2018-12-28 15:07:00 +0800

    Complete full_name_greeting exercise

--- week1/ch05/full_name_greeting.rb
+++ week1/ch05/full_name_greeting.rb
@@ -4,3 +4,10 @@
 
 
 ### Your Code Here ###
+puts "What\'s your first name?"
+first_name = gets.chomp
+puts "What\'s your middle name?"
+middle_name = gets.chomp
+puts "What\'s your last name?"
+last_name =  gets.chomp
+puts "Nice to meet you, " + first_name + " " + middle_name + " " + last_name + "!"
