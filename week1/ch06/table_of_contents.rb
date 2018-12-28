b7aa2fd6d0f6ea94e2304315bdc6ca969fba715d
scarlett7x <scarlett7x@hotmail.com>
2018-12-28 18:53:48 +0800

    Complete table_of_contents exercise

--- week1/ch06/table_of_contents.rb
+++ week1/ch06/table_of_contents.rb
@@ -2,3 +2,21 @@
 
 
 ### Your Code Here ###
+line_width = 80
+puts("Table of Contents".center(line_width))
+puts("Chapter  1: Getting Started".ljust(line_width) + "page   1".rjust(line_width))
+puts("Chapter  2: Numbers".ljust(line_width) + "page   9".rjust(line_width))
+puts("Chapter  3: Letters".ljust(line_width) + "page  13".rjust(line_width))
+puts("Chapter  4: Variables and Assignment".ljust(line_width) + "page  17".rjust(line_width))
+puts("Chapter  5: Mixing It Up".ljust(line_width) + "page  21".rjust(line_width))
+puts("Chapter  6: More About Methods".ljust(line_width) + "page  27".rjust(line_width))
+puts("Chapter  7: Flow Control".ljust(line_width) + "page  37".rjust(line_width))
+puts("Chapter  8: Arrays and Iterators".ljust(line_width) + "page  51".rjust(line_width))
+puts("Chapter  9: Writing Your Own Methods".ljust(line_width) + "page  57".rjust(line_width))
+puts("Chapter 10: There\'s Nothing New to Learn in Chapter 10".ljust(line_width) + "page  69".rjust(line_width))
+puts("Chapter 11: Reading and Writing, Saving and Loading, Yin and...Something Else".ljust(line_width) + "page  83".rjust(line_width))
+puts("Chapter 12: New Classes of Objects".ljust(line_width) + "page  95".rjust(line_width))
+puts("Chapter 13: Creating New Classes, Changing Existing Ones".ljust(line_width) + "page 103".rjust(line_width))
+puts("Chapter 14: Blocks and Procs".ljust(line_width) + "page 113".rjust(line_width))
+puts("Chapter 15: Beyond This Fine Book".ljust(line_width) + "page 123".rjust(line_width))
+puts("        A1: Possible Solutions".ljust(line_width) + "page 127".rjust(line_width))
