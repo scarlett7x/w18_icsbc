73f70a2f279a510071b5a9a8e3b18c80b18c6426
scarlett7x <scarlett7x@hotmail.com>
2019-01-03 20:21:51 +0800

    Complete deaf_grandma exercise

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,7 +1,14 @@
 puts 'COME GIVE GRANNY A KISS!'
 
-while () # Fill in the while condition
+while (true) # Fill in the while condition
   ### Your Code Here ###
+  answer = gets.chomp
+  year = 1930 + rand(21)
+  if answer != answer.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else if answer != "BYE"
+    puts "NO, NOT SINCE " + year.to_s + "!"
+       else puts 'OK SONNY, TALK AGAIN SOON'
+       end
+  end
 end
-
-puts 'OK SONNY, TALK AGAIN SOON'
