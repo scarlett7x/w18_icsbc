bc99c49ff541220a335c420e26489ca24f0612bf
scarlett7x <scarlett7x@hotmail.com>
2019-01-03 21:40:51 +0800

    Complete deaf_grandma_extended exercise

--- week2/ch07/deaf_grandma_extended.rb
+++ week2/ch07/deaf_grandma_extended.rb
@@ -1,8 +1,18 @@
 puts 'COME GIVE GRANNY A KISS!'
 
 bye_counter = 0
-while () # Fill in the while condition
+while (bye_counter < 3) # Fill in the while condition
   ### Your Code Here ###
+  answer = gets.chomp
+  year = 1930 + rand(21)
+  if answer == "BYE"
+    bye_counter = bye_counter + 1
+  else if answer != answer.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+       else puts "NO, NOT SINCE " + year.to_s + "!"
+       end
+       bye_counter = 0
+  end
 end
 
 puts 'OK SONNY, TALK AGAIN SOON'
