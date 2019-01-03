0adfc2704617afd52fc8af48535bc9ea88a7d743
scarlett7x <scarlett7x@hotmail.com>
2019-01-03 19:24:41 +0800

    Complete 99_bob exercise

--- week2/ch07/99_bob.rb
+++ week2/ch07/99_bob.rb
@@ -3,3 +3,16 @@
 
 
 ### Your Code Here ###
+number = 99
+while number > 1
+  puts number.to_s + " bottles of beer on the wall, " + number.to_s + " bottles of beer."
+  number = number - 1
+  puts "Take one down and pass it around, " + number.to_s + " bottles of beer on the wall."
+  if number == 1
+    break
+  end
+end
+puts "1 bottle of beer on the wall, 1 bottle of beer."
+puts "Take one down and pass it around, no more bottles of beer on the wall."
+puts "No more bottles of beer on the wall, no more bottles of beer."
+puts "Go to the store and buy some more, 99 bottles of beer on the wall."
