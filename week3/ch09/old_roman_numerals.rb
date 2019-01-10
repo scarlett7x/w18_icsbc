59506a94c52901bc62f8f0b0960f46f9ea31b080
scarlett7x <scarlett7x@hotmail.com>
2019-01-10 23:07:28 +0800

    update method name

--- week3/ch09/old_roman_numerals.rb
+++ week3/ch09/old_roman_numerals.rb
@@ -4,27 +4,24 @@ while (number  < 1 || number > 3000)
   puts "Please enter a value between 0 and 3001"
 end
 while (number > 0 && number < 3001)
+  mod_M = number % 1000
+  mod_D = number % 500
+  mod_C = number % 100
+  mod_L = number % 50
+  mod_X = number % 10
+  mod_V = number % 5
   num_M = number / 1000
-  rom = "M" * num_M
-  number = number % 1000
-  num_D =  number / 500
-  rom = rom + "D" * num_D
-  number = number % 500
-  num_C =  number / 100
-  rom = rom + "C" * num_C
-  number = number % 100
-  num_L =  number / 50
-  rom = rom + "L" * num_L
-  number = number % 50
-  num_X =  number / 10
-  rom = rom + "X" * num_X
-  number = number % 10
-  num_V =  number / 5
-  rom = rom + "V" * num_V
-  number = number % 5
-  num_I =  number
-  rom = rom + "I" * num_I
-  number = 0
+  num_D = mod_M / 500
+  num_C = mod_D / 100
+  num_L = mod_C / 50
+  num_X = mod_L / 10
+  num_V = mod_X / 5
+  num_I = mod_V
+  
+
+
+
+
   puts rom.to_s
   break
 end
